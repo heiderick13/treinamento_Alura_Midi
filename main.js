10415b94eb25e6b88645bb36c9197757210356f7
Marcelo Oliveira <marceloheiderick@outlook.com>
2022-09-03 12:36:45 -0300

    usando classlist e lendo melhor o dom

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
 for (let i = 0; i < listaDeTeclas.length; i++) {
-    
-    listaDeTeclas[i].onclick = tocaSomPom;
-}
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
 
+    // template string
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+}
